7259ed3980fea378b8ce74836faef53b9a522b27
Sammy Samkough <sammysamkough@gmail.com>
2020-05-18 23:13:28 -0400

    Fixed initial state

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,29 +1,39 @@
-import React, { useState } from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavigationBar = () => {
-  const [activeLink, setActiveLink] = useState(1);
+    const location = useLocation().pathname;
+    const initialState = location === '/list' ? 2 : 1;
 
-  const handleSelect = (eventKey: any) => setActiveLink(eventKey);
+    const [activeLink, setActiveLink] = useState(initialState);
 
-  return (
-    <div>
-      <Navbar>
-        <Navbar.Brand>
-          <h5>Notion Pretty Links 🔗</h5>
-        </Navbar.Brand>
-        <Nav activeKey={activeLink} onSelect={handleSelect}>
-          <Nav.Link as={Link} to="/" eventKey="1">
-            Replacer
-          </Nav.Link>
-          <Nav.Link as={Link} to="/list" eventKey="2">
-            List
-          </Nav.Link>
-        </Nav>
-      </Navbar>
-    </div>
-  );
+    const switchActive = (eventKey: any) => {
+        setActiveLink(eventKey);
+    };
+
+    return (
+        <div>
+            <Navbar>
+                <Navbar.Brand>
+                    <h6>
+                        Notion Pretty Links{' '}
+                        <span role="img" aria-label="link-emoji">
+                            🔗
+                        </span>
+                    </h6>
+                </Navbar.Brand>
+                <Nav activeKey={activeLink} onSelect={switchActive}>
+                    <Nav.Link as={Link} to="/" eventKey="1">
+                        Replacer
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/list" eventKey="2">
+                        List
+                    </Nav.Link>
+                </Nav>
+            </Navbar>
+        </div>
+    );
 };
 
 export default NavigationBar;
