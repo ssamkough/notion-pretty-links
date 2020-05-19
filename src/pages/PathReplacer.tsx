6c8131c65465a9803fd501b195c6bcafbbda0ff5
Sammy Samkough <sammysamkough@gmail.com>
2020-05-18 23:14:47 -0400

    Refactored path replacer page code

--- src/pages/PathReplacer.tsx
+++ src/pages/PathReplacer.tsx
@@ -1,44 +1,45 @@
-import React from "react";
-import { Row, Col, InputGroup, FormControl, Button } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Form, Button, Toast } from 'react-bootstrap';
 
 const PathList = () => {
-  return (
-    <div>
-      <Row>
-        <Col>
-          <label htmlFor="path-to-replace">Path to Replace</label>
-          <InputGroup className="mb-3">
-            <FormControl
-              id="path-to-replace"
-              placeholder="Path to Replace"
-              aria-label="Path-to-Replace"
-              aria-describedby="path-to-replace"
-            />
-          </InputGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <label htmlFor="new-path">New Path</label>
-          <InputGroup className="mb-3">
-            <FormControl
-              id="new-path"
-              placeholder="New Path"
-              aria-label="New-Path"
-              aria-describedby="new-path"
-            />
-          </InputGroup>
-        </Col>
-      </Row>
-      <Row className="float-right">
-        <Col>
-          <Button type="submit" value="Submit" variant="dark">
-            <span className="replace-btn">Replace</span>
-          </Button>
-        </Col>
-      </Row>
-    </div>
-  );
+    const [show, setShow] = useState(false);
+
+    return (
+        <div>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Path to Replace</Form.Label>
+                    <Form.Control
+                        id="path-to-replace-input"
+                        placeholder="Path to Replace"
+                        aria-label="Path-to-Replace"
+                        aria-describedby="path-to-replace"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>New Path</Form.Label>
+                    <Form.Control
+                        id="new-path-input"
+                        placeholder="New Path"
+                        aria-label="New-Path"
+                        aria-describedby="new-path"
+                    />
+                </Form.Group>
+                <div className="float-right">
+                    <Button id="replace-btn" onClick={() => setShow(true)} value="Submit" variant="dark">
+                        <span className="replace-btn-text">Replace</span>
+                    </Button>
+                </div>
+            </Form>
+
+            <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide className="added-path-toast">
+                <Toast.Header>
+                    <strong className="mr-auto">Added Path</strong>
+                </Toast.Header>
+                <Toast.Body>Path __________ added!</Toast.Body>
+            </Toast>
+        </div>
+    );
 };
 
 export default PathList;
