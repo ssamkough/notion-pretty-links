a912bfa2ff4179f92cc8ea81d123bc4651381251
Sammy Samkough <sammysamkough@gmail.com>
2020-05-18 23:13:47 -0400

    Refactored route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import NavigationBar from "./components/NavigationBar";
-import PathReplacer from "./pages/PathReplacer";
-import PathList from "./pages/PathList";
+import NavigationBar from './components/NavigationBar';
+import PathReplacer from './pages/PathReplacer';
+import List from './pages/List';
 
 const App = () => {
-  return (
-    <Router>
-      <Container id="app-outer-container">
-        <Container id="app-inner-container">
-          <Row>
-            <Col>
-              <NavigationBar />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Switch>
-                <Route exact path="/" component={PathReplacer} />
-                <Route path="/list" component={PathList} />
-              </Switch>
-            </Col>
-          </Row>
-        </Container>
-      </Container>
-    </Router>
-  );
+    return (
+        <Router>
+            <Container id="app-outer-container">
+                <Container id="app-inner-container">
+                    <Row>
+                        <Col>
+                            <NavigationBar />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Switch>
+                                <Route exact path="/">
+                                    <PathReplacer />
+                                </Route>
+                                <Route path="/list">
+                                    <List />
+                                </Route>
+                            </Switch>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+        </Router>
+    );
 };
 
 export default App;
