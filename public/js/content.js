4f781e3f20b44e3bd2eaef6ab2c228730c6a9ad8
Sammy Samkough <sammysamkough@gmail.com>
2020-05-18 23:13:09 -0400

    Updated content and popup

--- public/js/content.js
+++ public/js/content.js
@@ -1,41 +1,41 @@
+/* functions */
+function replacePath(event) {
+    console.log(event);
+    let replacedPath = document.getElementById('path-to-replace-input').innerHTML;
+    let newPath = document.getElementById('new-path-input').innerHTML;
+
+    console.log(replacedPath);
+    console.log(newPath);
+}
+/* end functions */
+
 function init() {
-  let fullUrl =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    "/" +
-    window.location.pathname;
-  console.log(fullUrl);
+    let fullUrl = window.location.protocol + '//' + window.location.host + '/' + window.location.pathname;
+    console.log(fullUrl);
 
-  let notionWorkspace = window.location.pathname.split("/")[1];
-  let notionLink =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    "/" +
-    notionWorkspace;
+    let notionWorkspace = window.location.pathname.split('/')[1];
+    let notionLink = window.location.protocol + '//' + window.location.host + '/' + notionWorkspace;
 
-  let notionSpecifiedLink =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    "//" +
-    notionWorkspace;
+    let notionSpecifiedLink = window.location.protocol + '//' + window.location.host + '//' + notionWorkspace;
 
-  let testLink =
-    notionSpecifiedLink + "/" + "Sandbox-af9c4fcd132f4fedbf80c30ab197023d";
+    let testLink = notionSpecifiedLink + '/' + 'Sandbox-af9c4fcd132f4fedbf80c30ab197023d';
 
-  if (testLink == fullUrl) {
-    console.log("got em");
-  }
+    if (testLink == fullUrl) {
+        console.log('got em');
+    }
 
-  chrome.runtime.onMessage.addListener(main);
+    // listeners
+    chrome.runtime.onMessage.addListener(main);
 }
 
 init();
 
 function main(message, sender, sendResponse) {
-  if (message.code === "gotUrl") {
-    console.log(message.url);
-  }
+    if (message.code === 'gotUrl') {
+        console.log(message.url);
+    }
+
+    if (message.code === 'test') {
+        console.log('btn');
+    }
 }
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,11 +1,22 @@
-window.addEventListener("load", loadPopup);
+window.addEventListener('load', function () {
+    // init
+    loadPopup;
+
+    // listeners
+    let replaceButton = document.getElementById('replace-btn');
+    // replaceButton.addEventListener('click', replacePath);
+});
 
 function loadPopup() {
-  let params = { active: true, lastFocusedWindow: true };
-  chrome.tabs.query(params, getUrl);
+    let params = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(params, getUrl);
 }
 
 function getUrl(tabs) {
-  let url = tabs[0].url;
-  chrome.tabs.sendMessage(tabs[0].id, { code: "gotUrl", link: url });
+    let url = tabs[0].url;
+    chrome.tabs.sendMessage(tabs[0].id, { code: 'gotUrl', link: url });
+}
+
+function replacePath() {
+    chrome.tabs.sendMessage(tabs[0].id, { code: 'test' });
 }
