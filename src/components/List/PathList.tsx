4b0e8c96150ac02cf75e6eaf09d46c8438238838
Sammy Samkough <sammysamkough@gmail.com>
2020-05-19 23:48:44 -0400

    Instead of mapping prop changed it to Object.keys() since it's an object and not an array

--- src/components/List/PathList.tsx
+++ src/components/List/PathList.tsx
@@ -4,14 +4,18 @@ import { Row, Col } from 'react-bootstrap';
 import PathItem from './PathItem';
 
 const PathList = ({ pathItems }: { pathItems: any }) => {
+    console.log('pathList page', pathItems);
     return (
         <div>
             <Row>
-                <Col>
-                    {pathItems &&
-                        pathItems.map((pathItem: any, e: number) => {
-                            return <PathItem pathItem={pathItem} key={e} />;
-                        })}
+                <Col id="path-item-list">
+                    {Object.keys(pathItems).map((key: any, e: number) => (
+                        <PathItem key={e}>
+                            {key}
+                            {': '}
+                            {pathItems[key]}
+                        </PathItem>
+                    ))}
                 </Col>
             </Row>
         </div>
