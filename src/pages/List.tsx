decd0911f8bc95faf8fc21b3686df529b23402f0
Sammy Samkough <sammysamkough@gmail.com>
2020-05-19 23:49:04 -0400

    Added useEffect, useState since we're accessing chrome.storage

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PathList from '../components/List/PathList';
 
 const List = () => {
+    const [pathData, setPathData] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
     const mockData = [
         { replaced_path: 'replacedpath1', new_path: 'newpath1' },
         { replaced_path: 'replacedpath2', new_path: 'newpath2' },
@@ -12,9 +16,37 @@ const List = () => {
         { replaced_path: 'replacedpath6', new_path: 'newpath6' },
     ];
 
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsError(false);
+            setIsLoading(true);
+
+            try {
+                await chrome.storage.sync.get(null, function (items: any) {
+                    console.log(items);
+                    setPathData(items);
+                    console.log(pathData);
+                });
+            } catch (error) {
+                setIsError(true);
+                console.log('Error:\n', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className="list-page">
-            <PathList pathItems={mockData} />
+            {isError ? (
+                <div>Something went wrong...</div>
+            ) : Object.keys(pathData).length === 0 ? (
+                <div>Loading...</div>
+            ) : (
+                <PathList pathItems={pathData} />
+            )}
         </div>
     );
 };
