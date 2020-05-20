5e82efcc77b1b4027906528c54da940db8641d3a
Sammy Samkough <sammysamkough@gmail.com>
2020-05-19 23:47:36 -0400

    Got more things to function such as replacing paths, adding new paths, putting mock data, etc.

--- public/js/content.js
+++ public/js/content.js
@@ -1,4 +1,27 @@
 /* functions */
+function loadPaths() {
+    console.log('loading');
+
+    chrome.storage.sync.get(null, function (items) {
+        // let fullUrl = window.location.protocol + '//' + window.location.host + '/' + window.location.pathname;
+        // let notionWorkspace = window.location.pathname.split('/')[1];
+
+        // let notionLink = window.location.protocol + '//' + window.location.host + '/' + notionWorkspace;
+        // let notionSpecifiedLink = window.location.protocol + '//' + window.location.host + '//' + notionWorkspace;
+        
+        console.log(items);
+        for (let [key, value] of Object.entries(items)) {
+            console.log(`${key}: ${value}`);
+            // let testLink = notionSpecifiedLink + '/' + value;
+            // if (testLink == fullUrl) {
+            //     console.log(`Got ${key}: ${value}`);
+            //     console.log('got em');
+            //     window.history.pushState("test", '', key);
+            // }
+        }
+    });
+}
+
 function replacePath(event) {
     console.log(event);
     let replacedPath = document.getElementById('path-to-replace-input').innerHTML;
@@ -7,22 +30,17 @@ function replacePath(event) {
     console.log(replacedPath);
     console.log(newPath);
 }
-/* end functions */
+
+function removePath() {
+
+}
+/* functions */
 
 function init() {
-    let fullUrl = window.location.protocol + '//' + window.location.host + '/' + window.location.pathname;
-    console.log(fullUrl);
+    console.log('init');
 
-    let notionWorkspace = window.location.pathname.split('/')[1];
-    let notionLink = window.location.protocol + '//' + window.location.host + '/' + notionWorkspace;
-
-    let notionSpecifiedLink = window.location.protocol + '//' + window.location.host + '//' + notionWorkspace;
-
-    let testLink = notionSpecifiedLink + '/' + 'Sandbox-af9c4fcd132f4fedbf80c30ab197023d';
-
-    if (testLink == fullUrl) {
-        console.log('got em');
-    }
+    // functions
+    loadPaths();
 
     // listeners
     chrome.runtime.onMessage.addListener(main);
@@ -32,10 +50,28 @@ init();
 
 function main(message, sender, sendResponse) {
     if (message.code === 'gotUrl') {
-        console.log(message.url);
+        console.log(message.code);
     }
 
-    if (message.code === 'test') {
-        console.log('btn');
+    if (message.code === 'addPath') {
+        console.log(message.code);
+        
+        const replacedPath = message.replaced_path;
+        const newPath = message.new_path;
+
+        chrome.storage.sync.set({ "replaced_path": replacedPath, "new_path": newPath }, function () {
+            console.log('replaced path: ' + replacedPath + ", new path: " + newPath);
+        });
+    }
+
+    if (message.code === 'placeMockData') {
+        const replacedPaths = ["replacedPath1","replacedPath2","replacedPath3","replacedPath4"]
+        const newPaths = ["testValuePath1","testValuePath2","testValuePath3","testValuePath4"]
+
+        for (let i = 0; i < replacedPaths.length; i++) {
+            chrome.storage.sync.set({ "replaced_path": replacedPaths[i], "new_path": newPaths[i] }, function () {
+                console.log('replaced path: ' + replacedPaths[i] + ", new path: " + newPaths[i]);
+            });
+        }
     }
 }
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,22 +1,39 @@
 window.addEventListener('load', function () {
     // init
-    loadPopup;
+    loadPopup();
 
     // listeners
     let replaceButton = document.getElementById('replace-btn');
-    // replaceButton.addEventListener('click', replacePath);
+    replaceButton.addEventListener('click', replacePath);
+
+    let mockDataButton = document.getElementById('mock-data-btn');
+    mockDataButton.addEventListener('click', placeMockData);
 });
 
 function loadPopup() {
     let params = { active: true, lastFocusedWindow: true };
-    chrome.tabs.query(params, getUrl);
+    chrome.tabs.query(params, sendLoadPopup);
 }
 
-function getUrl(tabs) {
+function sendLoadPopup(tabs) {
     let url = tabs[0].url;
     chrome.tabs.sendMessage(tabs[0].id, { code: 'gotUrl', link: url });
 }
 
 function replacePath() {
-    chrome.tabs.sendMessage(tabs[0].id, { code: 'test' });
+    let params = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(params, sendReplacePath);
+}
+
+function sendReplacePath(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { code: 'addPath', replaced_path: 'testReplacedPath', new_path: 'testNewPath' });
+}
+
+function placeMockData() {
+    let params = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(params, sendPlaceMockData);
+}
+
+function sendPlaceMockData(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { code: 'placeMockData' });
 }
