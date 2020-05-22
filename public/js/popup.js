991603a3e4ea718cd2cd6f2a98706a8415af60a0
Sammy Samkough <sammysamkough@gmail.com>
2020-05-22 03:31:24 -0400

    Added a couple listeners

--- public/js/popup.js
+++ public/js/popup.js
@@ -6,8 +6,14 @@ window.addEventListener('load', function () {
     let replaceButton = document.getElementById('replace-btn');
     replaceButton.addEventListener('click', replacePath);
 
-    let mockDataButton = document.getElementById('mock-data-btn');
-    mockDataButton.addEventListener('click', placeMockData);
+    // let mockDataButton = document.getElementById('mock-data-btn');
+    // mockDataButton.addEventListener('click', placeMockData);
+
+    let navLinkReplacer = document.getElementById('nav-link-replacer');
+    navLinkReplacer.addEventListener('click', switchToReplacerView);
+
+    let navLinkList = document.getElementById('nav-link-list');
+    navLinkList.addEventListener('click', switchToListView);
 });
 
 function loadPopup() {
@@ -26,14 +32,28 @@ function replacePath() {
 }
 
 function sendReplacePath(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { code: 'addPath', replaced_path: 'testReplacedPath', new_path: 'testNewPath' });
+    console.log("sendReplacePath");
+    let replacedPath = document.getElementById('path-to-replace-input').value;
+    let newPath = document.getElementById('new-path-input').value;
+
+    chrome.tabs.sendMessage(tabs[0].id, { code: 'addPath', replaced_path: replacedPath, new_path: newPath });
+
+    replacedPath = '';
+    newPath = '';
 }
 
-function placeMockData() {
-    let params = { active: true, lastFocusedWindow: true };
-    chrome.tabs.query(params, sendPlaceMockData);
+function switchToListView() {
+    let listPage = document.getElementById('list-page');
+    let replacerPage = document.getElementById('replacer-page');
+
+    listPage.style.display = "inline";
+    replacerPage.style.display = "none";
 }
 
-function sendPlaceMockData(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { code: 'placeMockData' });
+function switchToReplacerView() {
+    let listPage = document.getElementById('list-page');
+    let replacerPage = document.getElementById('replacer-page');
+
+    listPage.style.display = "none";
+    replacerPage.style.display = "inline";
 }
