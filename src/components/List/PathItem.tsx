624d61cb4f91ddc409f149b27a93291fe7c2c503
Sammy Samkough <sammysamkough@gmail.com>
2020-05-22 03:29:38 -0400

    Updated list components

--- src/components/List/PathItem.tsx
+++ src/components/List/PathItem.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 
-const PathItem = (pathItem: any) => {
-    const item = pathItem.pathItem;
-    console.log('pathItem page: ', pathItem);
+const PathItem = (replacedPath: any, newPath: any) => {
+    console.log('replaced path: ', replacedPath);
+    console.log('new path: ', newPath);
+
+    let rPath = replacedPath.replacedPath;
+    let nPath = replacedPath.newPath;
+
+    const removeItem = () => {
+        chrome.storage.sync.remove(nPath, function () {
+            console.log('Removed ' + nPath + ', ' + rPath + '!');
+        });
+    };
 
     return (
         <div className="path-item-outer-container">
             <div className="path-item-inner-container">
                 <Row className="float-right">
                     <Col>
-                        <Button type="submit" value="Close" aria-label="Close" className="close" variant="light">
+                        <Button
+                            onClick={removeItem}
+                            type="submit"
+                            value="Close"
+                            aria-label="Close"
+                            className="close"
+                            variant="light"
+                        >
                             <span aria-hidden="true">&times;</span>
                         </Button>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <span className="path-item-key">Old Path:</span>{' '}
-                    </Col>
-                    <Col>
-                        <span>{pathItem.replaced_path}</span>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="path-item-key ml-3">
+                    <Col className="path-item-key">
                         <span>New Path:</span>{' '}
                     </Col>
                     <Col>
-                        <span>{pathItem.new_path}</span>
+                        <span>{rPath.substring(0, 7)}...</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <span className="path-item-key ml-3">Old Path:</span>{' '}
+                    </Col>
+                    <Col>
+                        <span>{nPath.substring(0, 7)}...</span>
                     </Col>
                 </Row>
             </div>
--- src/components/List/PathList.tsx
+++ src/components/List/PathList.tsx
@@ -3,18 +3,14 @@ import { Row, Col } from 'react-bootstrap';
 
 import PathItem from './PathItem';
 
-const PathList = ({ pathItems }: { pathItems: any }) => {
-    console.log('pathList page', pathItems);
+const PathList = ({ pathObj }: { pathObj: any }) => {
+    console.log('pathList page: ', pathObj);
     return (
         <div>
             <Row>
                 <Col id="path-item-list">
-                    {Object.keys(pathItems).map((key: any, e: number) => (
-                        <PathItem key={e}>
-                            {key}
-                            {': '}
-                            {pathItems[key]}
-                        </PathItem>
+                    {Object.keys(pathObj).map((key: any, e: number) => (
+                        <PathItem key={e} replacedPath={pathObj[key]} newPath={key} />
                     ))}
                 </Col>
             </Row>
