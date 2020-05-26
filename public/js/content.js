7936f9bc42f5a95bcca78d7ddf2c69de3f26b0a2
Sammy Samkough <sammysamkough@gmail.com>
2020-05-25 23:52:32 -0400

    Removed console logs, added ability to actually replace URL if it matches

--- public/js/content.js
+++ public/js/content.js
@@ -1,67 +1,69 @@
 /* functions */
-function loadPaths() {
-    console.log('loading');
+function urlChange() {
+    console.log("url is changed")
+}
 
+function loadPaths() {
     chrome.storage.sync.get(null, function (items) {
         let fullUrl = window.location.protocol + '//' + window.location.host + '/' + window.location.pathname;
         let notionWorkspace = window.location.pathname.split('/')[1];
         let notionSpecifiedLink = window.location.protocol + '//' + window.location.host + '//' + notionWorkspace;
         
-        console.log(items);
-        for (let [key, value] of Object.entries(items)) {
-            let testLink = notionSpecifiedLink + '/' + value;
-            
-            console.log("test link:", testLink);
-            console.log("full url:", fullUrl);
-            if (testLink == fullUrl) {
-                console.log(`Switched ${key}: ${value}`);
-                window.history.pushState(key, '', key);
+        // console.log(items);
+        document.onreadystatechange = function () {
+            if(document.readyState === "complete") {
+                for (let [key, value] of Object.entries(items)) {
+                    let testLink = notionSpecifiedLink + '/' + value;
+                    
+                    if (testLink == fullUrl) {
+                        // console.log(`Switched ${key}: ${value}`);
+                        window.history.pushState(key, '', key);
+                    }
+                }
             }
         }
     });
 }
 
 function replacePath(event) {
-    console.log(event);
     let replacedPath = document.getElementById('path-to-replace-input').innerHTML;
     let newPath = document.getElementById('new-path-input').innerHTML;
-
-    console.log(replacedPath);
-    console.log(newPath);
 }
 
-function removePath() {
-
-}
-/* functions */
+function removePath() {}
+/* end functions */
 
 function init() {
-    console.log('init');
-
     // functions
     loadPaths();
 
     // listeners
     chrome.runtime.onMessage.addListener(main);
+    // window.onhashchange = urlChange;
+    // window.addEventListener('hashchange', urlChange, false);
+    window.addEventListener('popstate', urlChange);
+    window.addEventListener('popstate', function (event) {
+        // Log the state data to the console
+        console.log(event.state);
+    });
 }
 
 init();
 
 function main(message, sender, sendResponse) {
-    console.log("main");
     if (message.code === 'gotUrl') {
-        console.log(message.code);
+        // console.log(message.code);
     }
 
     if (message.code === 'addPath') {
-        console.log(message.code);
+        // console.log(message.code);
         
         const key = message.new_path;
         let storageObj = {}
         storageObj[key] = message.replaced_path;
 
         chrome.storage.sync.set(storageObj, function () {
-            console.log("Replacing " + storageObj[key] + " with " + key + ".");
+            // console.log("Replacing " + storageObj[key] + " with " + key + ".");
         });
     }
 }
